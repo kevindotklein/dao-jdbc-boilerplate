78b3256d17810da83df785031ab3d5b1916aac9e
chinpanji <kevindotklein@gmail.com>
2023-01-20 00:03:27 -0300

    error handling in deleteById

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -94,7 +94,12 @@ public class SellerDaoJDBC implements SellerDao{
 			);
 			st.setInt(1, id);
 
-			st.executeUpdate();
+			int rowsAffected = st.executeUpdate();
+
+			if(rowsAffected == 0){
+				throw new DbException("unexpected error! no rows affected");
+			}
+
 		}catch(SQLException e){
 			throw new DbException(e.getMessage());
 		}finally{
